67cca43c1d2b565e4da0b09cf10850b600c3bee6
Soumya Jha <jha.soumya@gmail.com>
2023-04-22 11:58:49 -0700

    Valid Parentheses

---
+++ leetcode/src/main/java/com/leetcode/top50/ValidParentheses.java
@@ -0,0 +1,40 @@
+package com.leetcode.top50;
+
+import java.util.Stack;
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            switch (ch) {
+                case '{':
+                case '(':
+                case '[':
+                    stack.push(ch);
+                    break;
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{')
+                        return false;
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(')
+                        return false;
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[')
+                        return false;
+                    break;
+                default:
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        ValidParentheses vp = new ValidParentheses();
+        System.out.println(vp.isValid("()"));
+        System.out.println(vp.isValid("()[]{}"));
+        System.out.println(vp.isValid("(]"));
+    }
+}
