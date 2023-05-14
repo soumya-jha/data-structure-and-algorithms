7b05e642d9fedd314e18d791580a17f141a149de
Soumya Jha <62205102+soumya-jha@users.noreply.github.com>
2023-05-14 16:56:41 -0700

    First missing positive

--- educative/src/main/java/com/educative/grokking/cyclicsort/FirstMissingPositive.java
+++ educative/src/main/java/com/educative/grokking/cyclicsort/FirstMissingPositive.java
@@ -2,6 +2,8 @@ package com.educative.grokking.cyclicsort;
 
 public class FirstMissingPositive {
     public static void main(String[] args) {
+        System.out.println("{1,1} " + firstMissingPositiveInteger(new int[] {1,1}));
+        System.out.println("{1,2,3} " + firstMissingPositiveInteger(new int[] {1,2,3}));
         System.out.println("{-1,-2,-3,-4} " + firstMissingPositiveInteger(new int[] {-1,-2,-3,-4}));
         System.out.println("{1,2,3,5} " + firstMissingPositiveInteger(new int[] {1,2,3,5}));
         System.out.println("{55,22,52,100,1,3,-5} " + firstMissingPositiveInteger(new int[] {55,22,52,100,1,3,-5}));
@@ -11,7 +13,7 @@ public class FirstMissingPositive {
     public static int firstMissingPositiveInteger(int[] nums) {
         int idx = 0;
         while(idx<nums.length){
-            if(nums[idx]!=idx+1 && nums[idx]>0 && nums[idx]<nums.length){
+            if(nums[idx]!=idx+1 && nums[idx]>0 && nums[idx]<nums.length && nums[idx]!=nums[nums[idx]-1]) {
                 //swap to correct position
                 int temp = nums[nums[idx]-1];
                 nums[nums[idx]-1] = nums[idx];
